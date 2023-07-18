75c9a879eb0418a3821272ae9789a1996abed20f
Hugo MG Sung <personar95@naver.com>
2023-07-18 11:42:33 +0900

    test

---
+++ Basic-gradle/sb-gradle-orm/src/main/java/com/hugo83/sbgradleorm/SbGradleOrmApplication.java
@@ -0,0 +1,13 @@
+package com.hugo83.sbgradleorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbGradleOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbGradleOrmApplication.class, args);
+	}
+
+}
---
+++ Basic-gradle/sb-gradle-orm/src/test/java/com/hugo83/sbgradleorm/SbGradleOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugo83.sbgradleorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbGradleOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
